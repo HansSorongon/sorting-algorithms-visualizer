2b82d78b55687b4c72c6110b3bd3fa0384997ab9
HansSorongon <hanssorongonn@gmail.com>
2023-05-06 15:21:29 +0800

    major changes

--- additional_functions.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-def display_text(surface, text, location, size):
-    font = pygame.font.Font('pixel_font.ttf', size)
-
-    text = font.render(text, True, 'white')
-    text_rect = text.get_rect()
-    surface.blit(text, location)
